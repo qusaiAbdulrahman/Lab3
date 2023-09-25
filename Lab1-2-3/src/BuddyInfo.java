afa6e1baf74cc230fed0ffa92763810d281bf49a
qusai <qusai.abdulrahman7@gmail.com>
2023-09-25 12:02:11 -0400

    Added BuddyInfo class

---
+++ Lab1-2-3/src/BuddyInfo.java
@@ -0,0 +1,23 @@
+
+public class BuddyInfo {
+
+    private Integer phoneNumber;
+    private String name;
+    private String address;
+
+    public BuddyInfo(String name, String address, Integer phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("John","carleton",220);
+        String buddyName = buddy.getName();
+        System.out.println("Buddy's name: " + buddyName);
+    }
+}
+
